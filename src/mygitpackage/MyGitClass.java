197252ff0b40519c76afeadb6f7b432d53bb5d9c
vehri <basvehri70@gmail.com>
2021-04-03 20:28:27 +0300

    ilk mesaj

---
+++ src/mygitpackage/MyGitClass.java
@@ -0,0 +1,13 @@
+package mygitpackage;
+
+public class MyGitClass {
+
+	public static void main(String[] args) {
+		System.out.println("Version 1");
+		System.out.println("My second commit");
+		
+		System.out.println("My third commit");		
+	
+
+}
+}
