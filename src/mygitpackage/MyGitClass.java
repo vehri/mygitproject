15b5639a0d52361f93b90b8bb9fa40252397d430
vehri <79199676+vehri@users.noreply.github.com>
2021-04-04 20:32:09 +0300

    Update MyGitClass.java

--- src/mygitpackage/MyGitClass.java
+++ src/mygitpackage/MyGitClass.java
@@ -5,28 +5,16 @@ public class MyGitClass {
 	public static void main(String[] args) {
 		System.out.println("Version 1");
 		System.out.println("My second commit");
-		
-<<<<<<< HEAD
+
 		System.out.println("My third commit");
-		
-		
-		
-		
-		
-		
-		System.out.println("git hup 16 satır");	
-		
+
+		System.out.println("git hup 16 satır");
+
+		System.out.println("My third commit");
+		System.out.println("My third commit");
+		System.out.println("My third commit");
+		System.out.println("git hup 12 satır");
+		System.out.println("git hup 13 satır");
+
 	}
 }
-=======
-		System.out.println("My third commit");		
-	System.out.println("My third commit");
-	System.out.println("My third commit");	
-System.out.println("git hup 12 satır");
-System.out.println("git hup 13 satır");		
-		
-		
-		
-}
-}
->>>>>>> 3ec5de6a39bb3b07593b63c7bd276d7a7906a7cd
